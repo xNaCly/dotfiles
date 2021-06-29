c11100c41726be26dc22bb9793a10767c4b914c1
xnacly <matteogropp@protonmail.com>
2021-06-29 16:21:32 +0200

    fixed a bug in colors.py

--- i3/scripts/colors.py
+++ i3/scripts/colors.py
@@ -4,21 +4,17 @@ import json
 import re
 import os
 with open("/home/xnacly/.cache/wal/colors.json","r") as f:
-    f = json.loads(f.read()) 
+    f = json.loads(f.read())
     background = f["special"]["background"]
     foreground = f["special"]["foreground"]
     accent = f["colors"]["color2"]
-    
-
 with open("/home/xnacly/.config/dunst/dunstrc", "r") as f:
     content = f.read()
-
 with open("/home/xnacly/.config/dunst/dunstrc", "w") as f:
     content = re.sub(r'frame_color = ".+"',f'frame_color = "{accent}"', content)
     content = re.sub(r'foreground = ".+"',f'foreground = "{foreground}"', content)
     content = re.sub(r'background = ".+"',f'background = "{background}"', content)
     f.write(content)
-
 os.system("cp -rv ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/colors-rofi-dark.rasi")
 os.system("echo 'element-icon { size: 1.25em; }' >> ~/.config/rofi/colors-rofi-dark.rasi")
-os.system("killall dunst; notify-send 'restarted dunst, rofi, alacritty and i3 with applied colorscheme")
+os.system("killall dunst; notify-send 'restarted dunst, rofi, alacritty and i3 with applied colorscheme'")
--- i3/scripts/select.sh
+++ i3/scripts/select.sh
@@ -5,8 +5,8 @@ img=`ls -l ~/.config/wallpapers | awk '{ print $9 }' | dmenu -i -l 10 -nb $backg
 
 rtrn=$?
 if test "$rtrn" = "0"; then
-  wal -i ~/.config/wallpapers/$img
-  python3 ~/.config/i3/scripts/colors.py
+  exec wal -i ~/.config/wallpapers/$img
+  exec python3 ~/.config/i3/scripts/colors.py
 else
   exit
 fi

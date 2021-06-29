617949d5f3d3e2289fceb0ec0220731329738f21
xnacly <matteogropp@protonmail.com>
2021-06-29 14:10:17 +0200

    updated scripts to apply fore- and background to dunst

--- i3/scripts/colors.py
+++ i3/scripts/colors.py
@@ -4,13 +4,19 @@ import json
 import re
 import os
 with open("/home/xnacly/.cache/wal/colors.json","r") as f:
-    color = json.loads(f.read())["colors"]["color2"]
+    f = json.loads(f.read()) 
+    background = f["special"]["background"]
+    foreground = f["special"]["foreground"]
+    accent = f["colors"]["color2"]
+    
 
 with open("/home/xnacly/.config/dunst/dunstrc", "r") as f:
     content = f.read()
 
 with open("/home/xnacly/.config/dunst/dunstrc", "w") as f:
-    content = re.sub(r'frame_color = ".+"',f'frame_color = "{color}"', content)
+    content = re.sub(r'frame_color = ".+"',f'frame_color = "{accent}"', content)
+    content = re.sub(r'foreground = ".+"',f'foreground = "{foreground}"', content)
+    content = re.sub(r'background = ".+"',f'background = "{background}"', content)
     f.write(content)
 
 os.system("killall dunst; notify-send 'restarted dunst with applied color'")

e669084b5a86ebe806ff1020fe6c9e66f8b848f2
xnacly <matteogropp@gmail.com>
2021-07-01 11:42:29 +0200

    added i3status to color.py

--- i3/scripts/colors.py
+++ i3/scripts/colors.py
@@ -1,14 +1,17 @@
-# script to read the accent color and apply it to dunst (notification frame)
+# a script to read the colors from pywal and insert these values into the dunst and i3status config 
 
 import json
 import re
 import os
 from time import sleep as s
+
 with open("/home/xnacly/.cache/wal/colors.json","r") as f:
     f = json.loads(f.read())
     background = f["special"]["background"]
     foreground = f["special"]["foreground"]
     accent = f["colors"]["color2"]
+    warning = f["colors"]["color9"]
+    text_color1 = f["colors"]["color6"]
 
 with open("/home/xnacly/.config/dunst/dunstrc", "r") as f:
     content = f.read()
@@ -19,7 +22,18 @@ with open("/home/xnacly/.config/dunst/dunstrc", "w") as f:
     content = re.sub(r'background = ".+"',f'background = "{background}"', content)
     f.write(content)
 
-s(1)
+with open("/home/xnacly/.config/i3status/i3status.conf", "r") as f:
+    content = f.read()
+
+with open("/home/xnacly/.config/i3status/i3status.conf", "w") as f:
+    content = re.sub(r'color_good = ".+"',f'color_good = "{text_color1}"', content)
+    content = re.sub(r'color_degraded = ".+"',f'color_degraded = "{warning}"', content)
+    content = re.sub(r'color_bad = ".+"',f'color_bad = "{warning}"', content)
+    print(content)
+    f.write(content)
+
+s(0.25)
 os.system("cp -rv ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/colors-rofi-dark.rasi")
 os.system("echo 'element-icon { size: 1.25em; }' >> ~/.config/rofi/colors-rofi-dark.rasi")
+os.system("i3 reload && i3 restart")
 os.system("killall dunst; notify-send 'restarted dunst, rofi, alacritty and i3 with applied colorscheme'")

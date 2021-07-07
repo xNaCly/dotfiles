1a0fc45dc1bd2fcef5893c670e4202a46182b6f5
xnacly <matteogropp@gmail.com>
2021-07-07 15:39:53 +0200

    Update install_packages.sh

--- install_scripts/install_packages.sh
+++ install_scripts/install_packages.sh
@@ -3,6 +3,6 @@ for package in $packages
 do 
   sudo pacman -S $package
 done
-yay -S lxapperance 
+yay -S lxappearance 
 sudo python3 -m pip install pywal || sudo pip install pywal;
 echo "installed all packages specified in '~/.config/install_scripts/packages.txt' + pywal & lxapperance"

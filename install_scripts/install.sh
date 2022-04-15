89bc70e7673c4eafb828b42e8554aa7ef106fe9b
xnacly <matteogropp@protonmail.com>
2022-04-15 15:24:27 +0200

    Update install.sh

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -1,15 +1,11 @@
 echo "Installing specified packages"
-packages=`cat ~/.config/install_scripts/packages.txt`
+packages=`cat $HOME/.config/install_scripts/packages.txt`
 for package in $packages
 do 
   sudo pacman -S $package --noconfirm
 done
-echo "Installed all packages specified in '~/.config/install_scripts/packages.txt"
+echo "Installed all packages specified in '$HOME/.config/install_scripts/packages.txt"
 
 echo "Moving Fonts"
-mkdir ~/.local/share/fonts/
-cp -rv ~/.config/fonts/* ~/.local/share/fonts/
-
-echo "Running Post install script"
-sudo pip install pywal;
-wal -t -s -i $HOME/.config/wallpapers/unicat.png
+mkdir -p $HOME/.local/share/fonts/
+cp -rv $HOME/.config/fonts/* $HOME/.local/share/fonts/

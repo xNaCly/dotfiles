b3b9fe1994ba3bc9cb47cfe81c416bb4b6b9d833
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-11-07 08:29:39 +0100

    fish: cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,68 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+#██╗  ██╗███╗   ██╗ █████╗  ██████╗██╗  ██╗   ██╗███████╗    ███████╗██╗███████╗██╗  ██╗     ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗ 
+#╚██╗██╔╝████╗  ██║██╔══██╗██╔════╝██║  ╚██╗ ██╔╝██╔════╝    ██╔════╝██║██╔════╝██║  ██║    ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝ 
+# ╚███╔╝ ██╔██╗ ██║███████║██║     ██║   ╚████╔╝ ███████╗    █████╗  ██║███████╗███████║    ██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗
+# ██╔██╗ ██║╚██╗██║██╔══██║██║     ██║    ╚██╔╝  ╚════██║    ██╔══╝  ██║╚════██║██╔══██║    ██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║
+#██╔╝ ██╗██║ ╚████║██║  ██║╚██████╗███████╗██║   ███████║    ██║     ██║███████║██║  ██║    ╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝
+#╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝╚══════╝╚═╝   ╚══════╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝ 
+# xnacly's fish config
+# https://github.com/xnacly
+
+### set configs ###
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+### Path extras ###
+export GOPATH="/home/$USER/.config/go"
+export PATH="$PATH:~/.config/go/bin"
+
+### aliases ###
+
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
 end
 
-# pnpm
-set -gx PNPM_HOME "/home/teo/.config/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
+function temp --description "Create and cd into temp directory"
+    set -l dir (mktemp --directory)
+    cd $dir
+end
+
+function temp_clone --description "Clone repo into temp dir"
+    temp
+    git clone $argv temprepo
+    cd temprepo
+end
+
+# extended shutdown
+alias ss="shutdown"
+
+# navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias cd..="cd .."
+
+# vim
+alias vim="nvim"
+
+# ls
+alias ls="exa"
+alias l="exa"
+alias la="exa"
+alias tree="exa --tree --level=4 --icons --git"
+
+# github
+alias gs="git status"
+alias ga="git add -A"
+alias gc="git commit -m"
+alias gps="git push"
+alias gpl="git pull"
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.config"
+export WAKATIME_HOME="$HOME/.config"
+
+function fish_greeting
+end

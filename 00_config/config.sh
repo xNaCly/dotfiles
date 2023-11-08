28ae5e51b093ca814807dbc73d302f338fd96c9c
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-11-08 08:55:01 +0100

    mehr: switch from scripts to mehr for system config

--- 00_config/config.sh
+++
@@ -1,6 +0,0 @@
-echo "@running config.sh"
-git config --global credential.helper store
-git config --global user.name "xnacly"
-git config --global user.email "47723417+xNaCly@users.noreply.github.com"
-echo "+configured git"
-
